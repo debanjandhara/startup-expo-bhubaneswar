aa166620befc4a0ccbdccdd86c6fc16788641a46
Debanjan Dhara <dd.debanjandhara@gmail.com>
2025-03-30 20:35:08 +0530

    added config file for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,16 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  }
 }));
